af29130ca82adde6710240fe35ec6b6e157aa5ee
Cesar Andre Mera Castro <cesar.mera.castro@gmail.com>
2024-12-09 15:45:35 -0500

    Inicial

---
+++ src/main/java/com/mycompany/tarea2/Tarea2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea2;
+
+/**
+ *
+ * @author cesar
+ */
+public class Tarea2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
